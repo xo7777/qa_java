f96e2f1577329fd652f39d982c7d8be0d65af6a5
Vladimir Antonyuk <xo7777@yandex.ru>
2025-08-27 19:10:02 +0300

    add LionTest

---
+++ src/test/java/ru/praktikum/LionTest.java
@@ -0,0 +1,51 @@
+package ru.praktikum;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    @Mock
+    private Feline feline;
+
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        Lion lion = new Lion("Самец", feline);
+        int expected = 1;
+        int actual = lion.getKittens();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        Lion lion = new Lion("Самец", feline);
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        List<String> actual = lion.getFood();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkExceptionWhenIncorrectSex(){
+        try{
+            Lion lion = new Lion("Мужской", feline);
+        } catch (Exception e) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
+        }
+    }
+
+}
+
+
+
